ee8330765f8e8a19705e5257b0a166d4a7318fc5
noura <noura.tu19@gmail.com>
2024-10-05 02:53:46 +0300

    update style sign up and login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import image from '../assets/bg.png';
-import { Link, useNavigate } from 'react-router-dom'; // Import useNavigate
+import { Link, useNavigate } from 'react-router-dom'; 
 import axios from 'axios';
 
 const Login = () => {
@@ -9,6 +9,7 @@ const Login = () => {
     password: '',
   });
 
+  const [error, setError] = useState(''); // State for error message
   const navigate = useNavigate(); // Initialize useNavigate
 
   const handleChange = (e) => {
@@ -17,6 +18,7 @@ const Login = () => {
       ...prevData,
       [name]: value,
     }));
+    setError(''); // Clear error message when the user types
   };
 
   const handleSubmit = async (e) => {
@@ -32,12 +34,12 @@ const Login = () => {
         console.log('Login successful:', user);
         navigate('/home'); // Redirect to /home on successful login
       } else {
+        setError('Invalid email or password'); // Set error message
         console.error('Invalid email or password');
-        // Show error message to user
       }
     } catch (error) {
+      setError('Error logging in. Please try again.'); // Handle error
       console.error('Error logging in:', error);
-      // Handle error as needed
     }
   };
 
@@ -48,20 +50,25 @@ const Login = () => {
     >
       <div className="text-center py-5">
         <div className="flex items-center justify-center">
-          <svg fill="none" viewBox="0 0 24 24" className="w-12 h-12 text-white" stroke="currentColor" aria-label="User Icon">
+          <svg fill="none" viewBox="0 0 24 24" className="w-12 h-12 text-black" stroke="currentColor" aria-label="User Icon">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z"/>
           </svg>
         </div>
-        <h2 className="text-4xl text-white tracking-tight">Sign in to your account</h2>
+        <h2 className="text-4xl text-black tracking-tight">Sign in to your account</h2>
         <span className="text-sm">
-          or <Link to="/signup"><div className="text-white"> register a new account</div></Link>
+          or <Link to="/signup"><div className="text-red-500 font-bold text-xl hover:text-black"> register a new account</div></Link>
         </span>
       </div>
-      <div className="flex justify-center my-2 mx-4 md:mx-0">
-        <form className="w-full max-w-xl text-white rounded-lg shadow-md p-6" onSubmit={handleSubmit}>
+      <div className="flex justify-center my-2 mx-4 md:mx-0 ">
+        <form className="w-full max-w-xl text-black rounded-lg shadow-md p-6 bg-red-200" onSubmit={handleSubmit}>
+          {error && (
+            <div className="mb-4 text-red-600 text-center">
+              {error}
+            </div>
+          )}
           <div className="flex flex-wrap -mx-3 mb-6">
             <div className="w-full md:w-full px-3 mb-6">
-              <label className="block uppercase tracking-wide text-white text-xs font-bold mb-2" htmlFor="email">Email address</label>
+              <label className="block uppercase tracking-wide text-black text-xs font-bold mb-2" htmlFor="email">Email address</label>
               <input
                 className="appearance-none block w-full text-gray-900 font-medium border border-gray-400 rounded-lg py-3 px-3 leading-tight focus:outline-none"
                 type="email"
@@ -73,7 +80,7 @@ const Login = () => {
               />
             </div>
             <div className="w-full md:w-full px-3 mb-6">
-              <label className="block uppercase tracking-wide text-white text-xs font-bold mb-2" htmlFor="password">Password</label>
+              <label className="block uppercase tracking-wide text-black text-xs font-bold mb-2" htmlFor="password">Password</label>
               <input
                 className="appearance-none block w-full bg-white text-gray-900 font-medium border border-gray-400 rounded-lg py-3 px-3 leading-tight focus:outline-none"
                 type="password"
@@ -87,14 +94,14 @@ const Login = () => {
             <div className="w-full flex items-center justify-between px-3 mb-3">
               <label htmlFor="remember" className="flex items-center w-1/2">
                 <input type="checkbox" id="remember" className="mr-1 bg-white shadow" />
-                <span className="text-sm text-white pt-1">Remember Me</span>
+                <span className="text-sm text-black pt-1">Remember Me</span>
               </label>
               <div className="w-1/2 text-right">
-                <a href="#" className="text-white text-sm tracking-tight">Forget your password?</a>
+                <a href="#" className="text-black text-sm tracking-tight">Forget your password?</a>
               </div>
             </div>
             <div className="w-full md:w-full px-3 mb-6">
-              <button className="appearance-none block w-full text-gray-100 font-bold border border-gray-200 rounded-lg py-3 px-3 leading-tight hover:bg-gray-200 hover:text-black focus:outline-none focus:bg-white focus:border-gray-500" type="submit">Sign in</button>
+              <button className="appearance-none block w-full text-black font-bold border border-red-500 rounded-lg py-3 px-3 leading-tight hover:bg-white hover:text-red-500 focus:outline-none focus:bg-white focus:border-gray-500" type="submit">Sign in</button>
             </div>
           </div>
         </form>
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import image from '../assets/bg.png';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom'; 
 import axios from 'axios';
 
 const SignUp = () => {
@@ -10,21 +10,42 @@ const SignUp = () => {
     password: ''
   });
 
+  const [error, setError] = useState('');  
+  const navigate = useNavigate();  
+
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData((prevData) => ({
       ...prevData,
       [name]: value
     }));
+    setError('');  
+  };
+
+  const validateEmail = (email) => {
+    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return re.test(String(email).toLowerCase());
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     
+    // Check if the name is more than 4 characters
+    if (formData.name.length < 4) {
+      setError('Name must be more than 4 characters.');
+      return;
+    }
+
+    // Validate email format
+    if (!validateEmail(formData.email)) {
+      setError('Please enter a valid email address.');
+      return;
+    }
+
     try {
       const response = await axios.post('https://66f168d9415379191550c6e8.mockapi.io/product', formData);
       console.log('Sign up successful:', response.data);
-      // Redirect or update UI as needed
+      navigate('/login'); // Redirect to login page after successful signup
     } catch (error) {
       console.error('Error signing up:', error);
       // Handle error as needed
@@ -38,20 +59,20 @@ const SignUp = () => {
     >
       <div className="text-center py-5">
         <div className="flex items-center justify-center">
-          <svg fill="none" viewBox="0 0 24 24" className="w-12 h-12 text-white" stroke="currentColor" aria-label="User Icon">
+          <svg fill="none" viewBox="0 0 24 24" className="w-12 h-12 text-black" stroke="currentColor" aria-label="User Icon">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z"/>
           </svg>
         </div>
-        <h2 className="text-4xl text-white tracking-tight">Create a new account</h2>
+        <h2 className="text-4xl text-black tracking-tight">Create a new account</h2>
         <span className="text-sm">
-          or <Link to="/login"><div className="text-white"> Log In</div></Link>
+          or <Link to="/login"><div className="text-red-500 font-bold text-xl hover:text-black"> Log In</div></Link>
         </span>
       </div>
       <div className="flex justify-center my-2 mx-4 md:mx-0">
-        <form className="w-full max-w-xl text-white rounded-lg shadow-md p-6" onSubmit={handleSubmit}>
+        <form className="w-full max-w-xl text-black rounded-lg shadow-md p-6 bg-red-200" onSubmit={handleSubmit}>
           <div className="flex flex-wrap -mx-3 mb-6">
             <div className="w-full md:w-full px-3 mb-6">
-              <label className="block uppercase tracking-wide text-white text-xs font-bold mb-2" htmlFor="name">Full Name</label>
+              <label className="block uppercase tracking-wide text-black text-xs font-bold mb-2" htmlFor="name">Full Name</label>
               <input
                 className="appearance-none block w-full bg-white text-gray-900 font-medium border border-gray-400 rounded-lg py-3 px-3 leading-tight focus:outline-none"
                 type="text"
@@ -63,7 +84,7 @@ const SignUp = () => {
               />
             </div>
             <div className="w-full md:w-full px-3 mb-6">
-              <label className="block uppercase tracking-wide text-white text-xs font-bold mb-2" htmlFor="email">Email address</label>
+              <label className="block uppercase tracking-wide text-black text-xs font-bold mb-2" htmlFor="email">Email address</label>
               <input
                 className="appearance-none block w-full bg-white text-gray-900 font-medium border border-gray-400 rounded-lg py-3 px-3 leading-tight focus:outline-none"
                 type="email"
@@ -75,7 +96,7 @@ const SignUp = () => {
               />
             </div>
             <div className="w-full md:w-full px-3 mb-6">
-              <label className="block uppercase tracking-wide text-white text-xs font-bold mb-2" htmlFor="password">Password</label>
+              <label className="block uppercase tracking-wide text-black text-xs font-bold mb-2" htmlFor="password">Password</label>
               <input
                 className="appearance-none block w-full bg-white text-gray-900 font-medium border border-gray-400 rounded-lg py-3 px-3 leading-tight focus:outline-none"
                 type="password"
@@ -86,14 +107,19 @@ const SignUp = () => {
                 required
               />
             </div>
+            {error && (
+              <div className="w-full text-red-500 px-3 mb-6 text-center">
+                {error}
+              </div>
+            )}
             <div className="w-full flex items-center justify-between px-3 mb-3">
               <label htmlFor="terms" className="flex items-center w-1/2">
                 <input type="checkbox" id="terms" className="mr-1 bg-white shadow" required />
-                <span className="text-sm text-white pt-1">I agree to the terms and conditions</span>
+                <span className="text-sm text-black pt-1">I agree to the terms and conditions</span>
               </label>
             </div>
             <div className="w-full md:w-full px-3 mb-6">
-              <button className="appearance-none block w-full text-gray-100 font-bold border border-gray-200 rounded-lg py-3 px-3 leading-tight hover:bg-gray-200 hover:text-black focus:outline-none focus:bg-white focus:border-gray-500" type="submit">Sign Up</button>
+            <button className="appearance-none block w-full text-black font-bold border border-red-500 rounded-lg py-3 px-3 leading-tight hover:bg-white hover:text-red-500 focus:outline-none focus:bg-white focus:border-gray-500" type="submit">Sign in</button>
             </div>
           </div>
         </form>
